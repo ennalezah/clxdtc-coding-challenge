b6eb902615ab0626fd0e06404cb4eb38265110a4
Hazel Anne Villareal <hae.villareal@gmail.com>
2021-01-31 12:55:53 -0800

    First commit

---
+++ app.js
@@ -0,0 +1,29 @@
+/********** 
+
+- This program should run from the command line, using NodeJs 
+- The program should print the numbers 1 to 100
+- Multiple of 3, print "CLX" instead of the number
+- Multiple of 5, print "DTC"
+- Multiple of 3 and 5, print "CLXDTC" 
+
+Example Output:
+
+1
+2
+CLX
+4
+DTC
+CLX
+7
+8
+CLX
+DTC
+11
+CLX
+13
+14
+CLXDTC
+
+**********/
+
+// console.log("Connected!")
