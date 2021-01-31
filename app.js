42381ca67de72abfb4a2faa5b0e2847338f0299b
Hazel Anne Villareal <hae.villareal@gmail.com>
2021-01-31 13:06:13 -0800

    Created while loop to print 1-15 as test run

--- app.js
+++ app.js
@@ -27,3 +27,20 @@ CLXDTC
 **********/
 
 // console.log("Connected!")
+
+let i = 1;
+const n = 16;
+
+while (i < n) {
+   if (i % 3 === 0 && i % 5===0) {
+      console.log("CLXDTC")
+   } else if(i % 3 === 0) {
+      console.log("CLX")
+   } else if(i % 5 === 0) {
+      console.log("DTC")
+   } else {
+      console.log(i)   
+   }
+   
+   i++;
+}
