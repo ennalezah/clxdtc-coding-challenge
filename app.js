2838ec1fbe7821a7f6732bdf20a696bcaee80c5f
Hazel Anne Villareal <hae.villareal@gmail.com>
2021-01-31 23:33:42 -0800

    Added README.md

--- app.js
+++ app.js
@@ -1,31 +1,3 @@
-/********** 
-
-- This program should run from the command line, using NodeJs 
-- The program should print the numbers 1 to 100
-- Multiple of 3, print "CLX" instead of the number
-- Multiple of 5, print "DTC"
-- Multiple of 3 and 5, print "CLXDTC" 
-
-Example Output:
-
-1
-2
-CLX
-4
-DTC
-CLX
-7
-8
-CLX
-DTC
-11
-CLX
-13
-14
-CLXDTC
-
-**********/
-
 // console.log("Connected!")
 
 const multipleOf3 = (num) => num % 3 === 0;
